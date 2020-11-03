1508fe6aaa5bdae15e84c3d6b3752553185cee6f
Ivan Hernandez Ruiz <musedelick70@gmail.com>
2020-11-03 17:23:13 -0600

    se añadieron archivos paara la conexion a bd

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import express from 'express'
 import morgan from 'morgan'
+import bd from './connectionBD'
 
 const app=express();
 app.use(morgan('dev'));
 
-app.get('/',(req,res)=>{
+app.get('/', async (req,res)=>{
     console.log('get from main');
+    
+    
     res.json('uwu');
 })
 
---
+++ src/config.js
@@ -0,0 +1,10 @@
+
+const database={
+    host:'db-pruebas.c3epzqfm1xrt.us-east-2.rds.amazonaws.com',
+    user:'admin',
+    password:'proyectopruebas2020',
+    port:'3306',
+    database:'papeleria'
+};
+
+export {database};
