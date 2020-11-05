afcc243da605038d373d193d1a4123b5da16d072
Ivan Hernandez Ruiz <musedelick70@gmail.com>
2020-11-05 12:30:18 -0600

    se añadio endpoint para obtener productos por id

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -7,11 +7,28 @@ export const createProduct= async (req,res)=>{
 }
 
 export const getAllproducts=async(req,res)=>{   
+    /*extraer esta logica de paginacion*/
     const itemsPerPage=15;
-    const {page}=req.params;
+    const page=parseInt(req.params.page);
     const offset=(page-1)*itemsPerPage;
-    console.log(page,itemsPerPage);
-    res.json({itemsPerPage,page,offset});
-    const productos =await (await dbcon()).query('select * from productos LIMIT ?, ?;',[page,itemPerPage]);
+    const countProducts= await (await dbcon()).query("select count(id_articulo) as 'items' from productos");
+    const totalproducts=countProducts[0].items;
+    const numberOfpages=Math.ceil(totalproducts/itemsPerPage);
+    /*fin logica de paginacion*/
+    const products =await (await dbcon()).query('select * from productos LIMIT ?, ?;',[offset,itemsPerPage]);
+    const resbody={
+        totalproducts,
+        page,
+        numberOfpages,
+        products
+    }
+    res.json(resbody);
+    //res.json(productos);
+}
+
+export const getproductsById=async(req,res)=>{   
+    const id= parseInt(req.params.id);
+    const product =await (await dbcon()).query('select * from productos WHERE id_articulo=?;',id);
+    res.json(product);
     //res.json(productos);
 }
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,10 +1,12 @@
 import {Router} from 'express'
-import {createProduct,getAllproducts} from '../controllers/products.controller'
+import {createProduct,getAllproducts,getproductsById} from '../controllers/products.controller'
 
 const router=Router();
 
-router.get('/',createProduct);
-router.get('/:page',getAllproducts);
+router.post('/',createProduct);
+router.get('/all/:page',getAllproducts);
+router.get('/:id',getproductsById);
+
 
 
 
